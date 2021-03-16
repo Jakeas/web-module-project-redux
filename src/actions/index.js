6c269eadef9548bc4d4ba79c34a5ba004d5f375c
Jakeas <JakeASierra@gmail.com>
2021-03-16 18:57:28 -0400

    working on figuring out what an action looks like

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+export { ADD_FEATURES } from './featureActions'
+export { REMOVE_FEATURES } from './featureActions'
+
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
 
 const AdditionalFeature = props => {
+  function handleClickAdd (){
+     return
+  }
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+
+      <button className="button" onClick={handleClickAdd}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
