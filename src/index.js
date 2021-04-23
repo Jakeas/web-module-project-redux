671c14651555cbeb1b919bf63759daf99b99d210
Jakeas <JakeASierra@gmail.com>
2021-04-23 12:52:18 -0400

    working on delete items bug

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import { reducer } from './reducers' 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import logger from 'redux-logger'
 
-
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger))
 console.log(store)
 
 const rootElement = document.getElementById('root');
